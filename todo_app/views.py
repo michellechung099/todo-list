5833276cc170d36970bf81ffc9cd5e456fae9ed7
michellechung099 <mchung099@gmail.com>
2024-03-28 18:03:33 +0900

    create views for lists

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView 
+from .models import ToDoList 
 
-# Create your views here.
+# display a list of to-do list titles
+class ListListView(ListView): 
+    # data model class thata you'd like to fetch
+    model = ToDoList 
+    # name of the template that will format the list into a displayable form
+    template_name = "todo_app/index.html"
+
+
+
+
+
+
